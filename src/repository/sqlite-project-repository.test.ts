6a54df2be687444a575bc3c0ac77749fef20c963
sangyeul <sangyeul.han@gmail.com>
2026-09-05 17:31:36 +0900

    test: cover hyphen-prefixed SQLite path

--- src/repository/sqlite-project-repository.test.ts
+++ src/repository/sqlite-project-repository.test.ts
@@ -8,10 +8,15 @@ import { Project } from "../domain/project.js";
 import { SQLiteProjectRepository } from "./sqlite-project-repository.js";
 
 const temporaryDirectories: string[] = [];
+const temporaryDatabaseFiles: string[] = [];
 
 afterEach(() => {
   for (const directory of temporaryDirectories.splice(0)) {
     rmSync(directory, { recursive: true, force: true });
+  }
+
+  for (const databaseFile of temporaryDatabaseFiles.splice(0)) {
+    rmSync(databaseFile, { force: true });
   }
 });
 
@@ -44,6 +49,17 @@ test("SQLite repository persists project names too large for a process argument"
   temporaryDirectories.push(directory);
   const repository = new SQLiteProjectRepository(join(directory, "projects.sqlite"));
   const project = new Project("large-project", "x".repeat(128 * 1024));
+
+  repository.save(project);
+
+  assert.deepEqual(repository.findById(project.id), project);
+});
+
+test("SQLite repository accepts a relative database path beginning with a hyphen", () => {
+  const databasePath = `-projects-${process.pid}.sqlite`;
+  temporaryDatabaseFiles.push(databasePath);
+  const repository = new SQLiteProjectRepository(databasePath);
+  const project = new Project("hyphen-path-project", "Hyphen path project");
 
   repository.save(project);
 
